90cd69ecc437a44e55c1c2f35bc555467e41758c
Roy Chavarría <roychav99@gmail.com>
2020-12-20 22:55:37 -0600

    Api y REACT

--- RestApi/src/index.js
+++ RestApi/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+
 const app = express();
 
-app.listen(3000);
+app.listen(5000);
 
-console.log('Server on port', 3000);
+app.get("/",function(req,res){
+    res.send("express here")
+})
+
+console.log('Server on port', 5000);
---
+++ webpage/src/getdata.js
@@ -0,0 +1,11 @@
+//import React, {Component} from 'react'
+
+export default class getData{
+    render() {
+        return(
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+        );
+    }
+}
