3922a6e43aa1c262af2dc4cee260570e803362c7
Roy Chavarría <roychav99@gmail.com>
2020-12-21 17:58:58 -0600

    Compose

--- Server/db/index.js
+++ Server/db/index.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/db', { useNewUrlParser: true })
+    .connect('mongodb://mongo:27017/db', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
